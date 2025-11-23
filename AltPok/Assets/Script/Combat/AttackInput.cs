ad9dfd44bd11ac61aaf9c58d64699b237c7a9406
SayedKibria <158774375+SayedKibria@users.noreply.github.com>
2025-11-23 15:06:40 -0500

    Delete AltPok/Assets/Script directory

--- AltPok/Assets/Script/Combat/AttackInput.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[System.Serializable]
-public struct AttackInput
-{
-    public KeyCode key;          // The key to press
-    public AttackData attackData; // The attack data to use
-}
